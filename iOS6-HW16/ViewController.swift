f248fbd5a018a0517937db2c2e63cd663e634a1f
DornPorn <gungan722121@gmail.com>
2022-06-23 14:58:08 +0700

    Initial Commit

---
+++ iOS6-HW16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-HW16
+//
+//  Created by Stanislav Rassolenko on 6/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
